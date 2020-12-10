4761ea57bb166cb98a0780850889cccda0b53bdb
Peter van Dijk <peter.van.dijk@bynder.com>
2020-12-10 14:47:44 +0100

    first test code

--- dashboard/client/src/App.tsx
+++ dashboard/client/src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Knob } from "./components/components/Knob";
+import { SocketConnect } from './lib/SocketConnect';
 const { useEffect, useState } = React;
 
 export const sleep = (time: number): Promise<unknown> =>
@@ -18,6 +19,7 @@ export const App = () => {
         <Knob step={1} min={117} max={137} resultValue={knob_1} setResultValue={setKnob_1} knobStep={10} />
         <Knob step={25} min={0} max={950} resultValue={knob_2} setResultValue={setKnob_2} knobStep={4} isInner/>
       </div>
+      <SocketConnect />
     </div>
   );
 };
---
+++ dashboard/client/src/lib/SocketConnect.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import socket class
+// @ts-ignore
+import Socket from './socket';
+
+
+export class SocketConnect extends React.Component {
+
+    constructor(props: any) {
+        super(props);
+
+        // state variables
+        this.state = {
+            connected: false,
+        }
+    }
+
+    // componentDidMount is a react life-cycle method that runs after the component 
+    //   has mounted.
+    componentDidMount() {
+        // establish websocket connection to backend server.
+        let ws = new WebSocket('ws://localhost:9000/ws');
+
+        // create and assign a socket to a variable.
+// @ts-ignore
+        let socket = this.socket = new Socket(ws);
+
+        // handle connect and discconnect events.
+        socket.on('connect', this.onConnect);
+        socket.on('disconnect', this.onDisconnect);
+
+        /* EVENT LISTENERS */
+        // event listener to handle 'hello' from a server
+        socket.on('helloFromServer', this.helloFromServer);
+    }
+
+    // onConnect sets the state to true indicating the socket has connected 
+    //    successfully.
+    onConnect = () => {
+        this.setState({connected: true});
+    }
+
+    // onDisconnect sets the state to false indicating the socket has been 
+    //    disconnected.
+    onDisconnect = () => {
+        this.setState({connected: false});
+    }
+
+    // helloFromClient is an event emitter that sends a hello message to the backend 
+    //    server on the socket.
+    helloFromClient = () => {
+        console.log('saying hello...');
+// @ts-ignore
+        this.socket.emit('helloFromClient', 'hello server!');
+    }
+
+    // helloFromServer is an event listener/consumer that handles hello messages 
+    //    from the backend server on the socket.
+    helloFromServer = (data: any) => {
+        console.log('hello from server! message:', data);
+    }
+
+
+    // render returns the JSX (UI elements).
+    //   h1 title
+    //   button that calls the event emitter helloFromClient.
+    render() {
+        const title = "Go Sockets Tutorial";
+
+        return (
+            <div>
+                <h1>{title}</h1>
+                <hr/>
+                <button onClick={this.helloFromClient}>
+                    Say Hello to Backend Server
+                </button>
+            </div>
+        )
+    }
+}
